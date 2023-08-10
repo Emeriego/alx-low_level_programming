7b86760df4f1662c92175173184d021fbc8e10c3
Godwin <emeriego@gmail.com>
2023-08-10 08:41:32 -0700

    printf added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  
+/**
+ * main - Entry Point.
+ *
+ * Description: This prints the string as indicated.
+ * Return: Always 0 if successful
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
