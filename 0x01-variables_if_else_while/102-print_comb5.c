47c55ed86f6ae3dc76ab5ae577de2cece12e027d
Godwin <emeriego@gmail.com>
2023-08-11 10:47:22 -0700

    man

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: This program prints combination of three numbers.
+ * Return: Always returns 0 if success.
+ */
+#include <stdio.h>
+int main(void)
+{
+	int i = 0;
+
+	while (i < 100)
+	{
+		int c = i + 1;
+
+		while (c < 100)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((c / 10) + '0');
+			putchar((c % 10) + '0');
+			if (i != 99 || c != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			c++;
+		}
+		i++;
+	}
+	return (0);
+}
