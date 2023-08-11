838735a15e3080896153130488c3d4ec3dcf1a06
Godwin <emeriego@gmail.com>
2023-08-11 07:26:02 -0700

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Description: This program prints hexadecimals.
+ * Return: Always returns 0 if success.
+ */
+int main(void)
+{
+	int i = 0;
+	
+	while (i <= 9)
+	{
+		int x = 0;
+
+		while (x <= 9)
+		{
+			if (!(i == x) && (i < x))
+			{
+				putchar(i + '0');
+				putchar(x + '0');
+				if (i < 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			x++;
+		}
+		putchar('\n');
+		i++;
+	}return 0;
+}
+
