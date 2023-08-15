7d80e275437aede241a0f740424f6bfbc882c9b7
Godwin <emeriego@gmail.com>
2023-08-15 07:29:51 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_sign - this does what you see.
+ * @n: The character to be checked.
+ *
+ * Description: This func does what it does.
+ * Return: returns 1 if success but 0 if false.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
