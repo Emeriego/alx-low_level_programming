0f2010939dc149b3fc8b8be3b6f87a6c9be635aa
Godwin <emeriego@gmail.com>
2023-08-15 09:12:07 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_sign - this does what you see.
+ * @n: The character to be checked.
+ *
+ * Description: This func does what it does.
+ * Return: returns 1 if success but 0 if false.
+ */
+int _abs(int n);
+{
+	int a_n;
+
+	a_n = abs(n);
+	return (a_n);
+{
