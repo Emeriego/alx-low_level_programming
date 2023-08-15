31db3d47b55607c2d522d6027f366bc3c0806dd2
Godwin <emeriego@gmail.com>
2023-08-15 04:11:14 -0700

    Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry Point.
  *
  * Description: This prints the string as indicated.
  * Return: Always 0 if successful
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
