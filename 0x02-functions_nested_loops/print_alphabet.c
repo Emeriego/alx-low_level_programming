389174ea7140d5586096d23f3498965d5db024c8
Godwin <emeriego@gmail.com>
2023-08-15 05:26:40 -0700

    first function

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point.
+ *
+ * Description: This func does what it does.
+ * Return: returns 0.
+ */
+int main(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+
+}
