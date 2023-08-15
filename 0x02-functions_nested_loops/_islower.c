fb04f5bf0641e2614002cfff4ee7191c0b2addd7
Godwin <emeriego@gmail.com>
2023-08-15 06:45:23 -0700

    is lower

---
+++ 0x02-functions_nested_loops/_islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _islower - this does what you see.
+ *
+ * Description: This func does what it does.
+ *
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
