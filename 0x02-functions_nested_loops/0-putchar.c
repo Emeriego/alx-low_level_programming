aa2b6a7d6a01d19faf87a4dfc57edf1dad958d2f
Godwin <emeriego@gmail.com>
2023-08-15 04:29:58 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,19 @@
-#ifndef MYHEADER_H
-#define MYHEADER_H
-
-	char _putchar(char);
-
-#endif
+#include "main.h"
+/**
+ * main - This is the entry point.
+ *
+ * Description: This code uses a user defined function to print _putchar
+ * Return: This would always return 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
