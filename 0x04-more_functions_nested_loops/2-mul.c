67ada8f06ba706754b9e4f6da86769a62f282b0f
Godwin <emeriego@gmail.com>
2023-08-17 04:13:23 -0700

    my multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - prints upper.
+ * @a: character to be converted.
+ * @b: character to be converted.
+ * Return: reurns 0 or 1.
+ */
+int mul(int a, int b)
+{
+	long int prd;
+
+	prd = a * b;
+	return (prd);
+}
+
