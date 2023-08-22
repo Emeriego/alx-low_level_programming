c26798031dc8e9f34d7d0b25114d04041d8201a1
Godwin <emeriego@gmail.com>
2023-08-21 20:42:14 -0700

    corrections

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * puts_half - This functions resets a value.
  * @str: This is a param.
@@ -9,7 +8,12 @@ void puts_half(char *str)
 {
 	int n, l;
 
-	l = strlen(str);
+	l = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
 	if ((l % 2) != 0)
 	{
 		n = (l - 1) / 2;
