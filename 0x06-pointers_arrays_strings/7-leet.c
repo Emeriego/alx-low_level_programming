816fe878f148d3c1b2fca6700311a02a154147d8
Godwin <emeriego@gmail.com>
2023-08-24 02:34:28 -0700

    corrections

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - This is a custom strncat.
+ * @s: First param.
+ * Return: returns a pointer to string.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	int j;
+	char alpha[] = "aAeEoOtTlL";
+	char digits[] = "4433007711";
+	
+	while (s[i] != '\0')
+	{
+		for (j=0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = digits[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
