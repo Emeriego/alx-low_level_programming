7d6233ab4015ed6e8e8f7445facd51cf483db6a4
Godwin <emeriego@gmail.com>
2023-08-28 16:32:27 -0700

    files added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - sets a double pointer.
+ * @s: first param.
+ * @to: second param.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
