2a9a0b3b1856e634f14a4afe77477a0f8c642788
Godwin <emeriego@gmail.com>
2023-09-01 16:08:29 -0700

    Arguments added

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * @argc: aregument count.
+ * @argv: array of arguments.
+ * Return: returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
