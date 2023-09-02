290c998ba9015d024248bb295b8d0bd60da01704
Godwin <emeriego@gmail.com>
2023-09-01 17:13:15 -0700

    added corections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int a, b, mul;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-		int a = atoi(argv[1]);
-		int b = atoi(argv[2]);
-		int mul = a * b;
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		mul = a * b;
 
 		printf("%d\n", mul);
+		return (0);
 }
