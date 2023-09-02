ff8021cc389a68b34757398b720ca9307152a820
Godwin <emeriego@gmail.com>
2023-09-01 18:05:26 -0700

    added corections

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point.
+ * @argc: aregument count.
+ * @argv: array of arguments.
+ * Return: returns 0.
+ */
+int main(int argc, char *argv[])
+{
+	int a, i, j, sum;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		a = atoi(argv[i]);
+		sum = sum + a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
