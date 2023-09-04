5b36a7a07fba1ed74874ca078eff006507989e4a
Godwin <emeriego@gmail.com>
2023-09-04 15:51:11 -0700

    Malloc continued

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,9 +8,12 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	for (i = 0; i <= height; i++)
+	if (grid != NULL && height != 0)
 	{
-		free(grid[height]);
+		for (i = 0; i <= height; i++)
+		{
+			free(grid[height]);
+		}
+		free(grid);
 	}
-	free(grid);
 }
