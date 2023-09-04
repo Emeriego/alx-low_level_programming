cd7636ecfe9def133b76be1c378c814d5470eb23
Godwin <emeriego@gmail.com>
2023-09-04 04:37:44 -0700

    Started Malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (0);
 	}
-	p = (char*)malloc(sizeof(char) * size);
+	p = malloc(sizeof(char) * size);
 	if (p == NULL)
 	{
 		return (NULL);
