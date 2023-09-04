4a5009530d562e9c5e5db89c92631c22c2a4c9e9
Godwin <emeriego@gmail.com>
2023-09-04 04:18:33 -0700

    Started Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - This Function creates an array of char.
+ * @size: first param.
+ * @c: second param.
+ * Return: returns apointer if succesful.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	int i;
+
+	if (size == 0)
+	{
+		return (0);
+	}
+	p = (char*)malloc(size);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+	return (p);
+}
