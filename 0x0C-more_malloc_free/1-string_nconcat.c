aa340a1aeda3b1775ad1ec6ceb5bd555cb0cc89f
Godwin <emeriego@gmail.com>
2023-09-07 10:35:42 -0700

    corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,14 +13,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int c, len;
 	unsigned int len_s1, len_s2, k = 0;
 
-	s1 = s1 == NULL ? '' : s1;
-	s2 = s2 == NULL ? '' : s2;
+	s1 = (s1 == NULL) ? "" : s1;
+	s2 = (s2 == NULL) ? "" : s2;
 	for (c = 0; s1[c] != '\0'; c++)
 		len_s1++;
-
 	for (c = 0; s2[c] != '\0'; c++)
 		len_s2++;
-
 	len = n >= len_s2 ? (len_s1 + len_s2 + 1) : (len_s1 + n + 1);
 	str = malloc(sizeof(char) * len);
 	if (str == NULL)
