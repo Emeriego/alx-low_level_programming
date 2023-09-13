63dd8ee97755ffb2afcd2ff328e07ac44cf2ce3e
Godwin <emeriego@gmail.com>
2023-09-13 09:09:15 -0700

    brain bending concept

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - calls a function passed into it.
+ * @name: name to be printed.
+ * @f: pointer to function to be called.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
