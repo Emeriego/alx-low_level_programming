8664afc34aa72c2b0a8e1c5a64abaf1f40e9b985
Godwin <emeriego@gmail.com>
2023-09-14 16:00:57 -0700

    some form of corrections

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,17 +8,14 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i, arr;
-
+	unsigned int i;
 	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		arr = va_arg(args, const unsigned int);
-		printf("%d", arr);
-
+		printf("%d", va_arg(args, unsigned int));
 		if (i != (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
