8c2a4d4c48498fdeaf4fcf211a5c7069036dbc6e
Godwin <emeriego@gmail.com>
2023-09-13 18:54:20 -0700

    3 files added

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: arguement count.
+ * @argv: arg array.
+ * Return: returns 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	result = operation(num1, num2);
+	printf("%d\n", result);
+	return (0);
+}
+
