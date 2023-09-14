bc00f05b312a9656a1788828ad076202d5f6d3ad
Godwin <emeriego@gmail.com>
2023-09-14 07:04:59 -0700

    print numbers task added

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers - This is variadic way of summing args.
+ * @separator: first arg.
+ * @n: second arg.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	size_t i;
+	va_list my_args;
+
+	va_start(my_args, n);
+
+	if (separator != NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d%c ", va_arg(my_args, unsigned int), *separator);
+		}
+		printf("%d\n", va_arg(my_args, unsigned int));
+	}
+}
