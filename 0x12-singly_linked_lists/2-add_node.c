e2b9413f269d1806e34a59d17a5d23de2dfa3396
Godwin <emeriego@gmail.com>
2023-09-21 08:34:00 -0700

    task 2 added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_head;
 
 	new_head = malloc(sizeof(list_t));
-	if (!new_head)
+	if (!new_head || !str)
 		return (NULL);
 
 	new_head->str = strdup(str);
