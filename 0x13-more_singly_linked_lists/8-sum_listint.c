f14886ea340eede4ed0b2931424a52f928285e90
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-26 11:15:01 +0100

    corrections

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * listint_len - prints singly lists.
+ * sum_listint - sums up singly lists.
  * @head: first parameter.
  * Return: returns the number of nodes.
  */
 int sum_listint(listint_t *head)
 {
 	const listint_t *current = head;
-    int sum = 0;
+	int sum = 0;
 
-    if (current == NULL)
-        return (0);
-
+	if (current == NULL)
+		return (0);
 	while (current != NULL)
 	{
-        sum = sum + current->n;
+		sum = sum + current->n;
 		current = current->next;
 	}
 	return (sum);
