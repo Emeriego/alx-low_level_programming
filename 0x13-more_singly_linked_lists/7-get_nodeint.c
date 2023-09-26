776661f225ee9e992142eec71c8609e3e1992639
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-26 11:11:55 +0100

    corrections

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,22 +4,22 @@
  * get_nodeint_at_index - pops the singly  linked  lists.
  * @head: first parameter.
  * @index: index being looked for.
+ * Return: returns address of node.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    listint_t *tmp_head = head;
-    size_t n;
+	listint_t *tmp_head = head;
+	size_t n;
 
-    if(tmp_head == NULL)
-        return (NULL);
-    
-    for (n = 0; n < index; n++)
-    {
-        if (tmp_head == NULL)
-        {
-            return (NULL);
-        }
-        tmp_head = tmp_head->next;
-    }
-    return (tmp_head);
+	if (tmp_head == NULL)
+		return (NULL);
+	for (n = 0; n < index; n++)
+	{
+		if (tmp_head == NULL)
+		{
+			return (NULL);
+		}
+		tmp_head = tmp_head->next;
+	}
+	return (tmp_head);
 }
