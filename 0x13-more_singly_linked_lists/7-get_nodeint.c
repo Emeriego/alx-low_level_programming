a298fbb8b2dfeef92f240223ba99eac28c7fe0ea
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-26 10:57:57 +0100

    corrections

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
     listint_t *tmp_head = head;
     size_t n;
 
-    if(tmp_head == NULL)
+    if(tmp_head == NULL || index < 0)
         return (NULL);
     
     for (n = 0; n < index; n++)
