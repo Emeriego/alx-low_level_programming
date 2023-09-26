2b50c1aa85f07da930e773a9e39e354ca7e4ae70
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-26 19:29:39 +0100

    corrections

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,21 +6,20 @@
  * @idx: index of the list where the new node is.
  * @n: integer element.
  *
- * Return: the address of the new node, or NULL if it
+ * Return: returns the address of the new node, or NULL if it
  * fails.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	size_t i;
-	listint_t *new_node;
 	listint_t *node_at_idx;
+	listint_t *new_node;
 
 	if (idx != 0)
-        node_at_idx = get_nodeint_at_index(*head, idx);
+		node_at_idx = get_nodeint_at_index(*head, idx - 1);
 
 	if (node_at_idx == NULL)
 		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -36,6 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new_node->next = node_at_idx->next;
 		node_at_idx->next = new_node;
 	}
-
 	return (new_node);
 }
