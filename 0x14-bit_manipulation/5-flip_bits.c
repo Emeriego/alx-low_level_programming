366f6564e9f8ba2ac1f7eaa0a19b18b23625bcde
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-29 00:30:06 +0100

    bit manipulation added

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,12 +8,12 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	size_t c = 0;
-    unsigned long int x_result = n ^ m;
+	unsigned long int x_result = n ^ m;
 
-    while (x_result > 0)
-    {
-        c = c + x_result & 1;
-        x_result = x_result >> 1;
-    }
+	while (x_result > 0)
+	{
+		c = c + x_result & 1;
+		x_result = x_result >> 1;
+	}
 	return (c);
 }
