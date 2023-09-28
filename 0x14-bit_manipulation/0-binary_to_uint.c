4978b2bb9a1b4847262d13a89f50701b7e5e351b
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-09-29 00:16:04 +0100

    bit manipulation added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,33 +6,33 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    size_t i, j, dec, pow, len;
-    int base = 2;
+	size_t i, j, dec, pow, len;
+	int base = 2;
 
-    i = 0;
-    j = 0;
-    dec = 0;
-    len = 0;
-    pow = 1;
+	i = 0;
+	j = 0;
+	dec = 0;
+	len = 0;
+	pow = 1;
 
-    if (!b)
-    {
-        return (0);
-    }
-    while (b[len] != '\0')
-        len++;
-    if (len == 1 && (b[0] == '0' || b[0] == '1'))
-        return (b[0] - '0');
-    while (b[i] != '\0')
-    {
-        if (b[i] != '0' || b[i] != '1')
-            return (0);
-        for (j = len - 1; j > 0; j--)
-            pow = pow * base;
-        dec = dec + (pow * (b[i] - '0'));
-        len --;
-        pow = 1;
-        i++;
-    }
-    return (dec);
+	if (!b)
+	{
+		return (0);
+	}
+	while (b[len] != '\0')
+		len++;
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+		return (b[0] - '0');
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+		for (j = len - 1; j > 0; j--)
+			pow = pow * base;
+		dec = dec + (pow * (b[i] - '0'));
+		len--;
+		pow = 1;
+		i++;
+	}
+	return (dec);
 }
