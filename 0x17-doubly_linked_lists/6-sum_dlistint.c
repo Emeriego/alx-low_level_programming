4a67835f891647ba55fb369840b5fb43bd49a5c5
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-11-10 03:26:54 +0100

    Doubly linked list tasked finally completed

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,9 +11,9 @@ int sum_dlistint(dlistint_t *head)
 	if (head != NULL)
 	{
 		while (head->prev != NULL)
-        {
+		{
 			head = head->prev;
-        }
+		}
 		while (head != NULL)
 		{
 			sum = sum + head->n;
